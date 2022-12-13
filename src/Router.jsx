44b0a9ea5e5168166d69f9963f4ad653d114b6ba
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-12-13 16:03:26 -0300

    feat: save work changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import App from './App';
 
-function Router() {
+export default function Router() {
   return (
     <Routes>
       <Route
@@ -11,19 +11,62 @@ function Router() {
         element={ <App /> }
       />
       <Route
-        path="cbmm"
-        element={ <App /> }
-      />
-      <Route
-        path="helbor"
-        element={ <App /> }
-      />
-      <Route
-        path="gpa"
+        path="/:page"
         element={ <App /> }
       />
     </Routes>
   );
 }
 
-export default Router;
+// export default Router;
+
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams
+// } from "react-router-dom";
+
+// // Params are placeholders in the URL that begin
+// // with a colon, like the `:id` param defined in
+// // the route in this example. A similar convention
+// // is used for matching dynamic segments in other
+// // popular web frameworks like Rails and Express.
+
+// export default function ParamsExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <h2>Accounts</h2>
+
+//         <ul>
+//           <li>
+//             <Link to="/netflix">Netflix</Link>
+//           </li>
+//           <li>
+//             <Link to="/zillow-group">Zillow Group</Link>
+//           </li>
+//           <li>
+//             <Link to="/yahoo">Yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to="/modus-create">Modus Create</Link>
+//           </li>
+//         </ul>
+
+//         <Switch>
+//           <Route path="/:id" children={<Child />} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Child() {
+//   // We can use the `useParams` hook here to access
+//     <div>
+//       <h3>ID: {id}</h3>
+//     </div>
+// }
