a91b1630cc76e63fda4becf69dde136c6ce560be
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-12-14 15:34:58 -0300

    feat: add visible and invisible eye to cards

--- src/components/customizationsCard.jsx
+++ src/components/customizationsCard.jsx
@@ -1,26 +1,68 @@
 import "../App.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function customizationsCard({ customization }) {
+  const [hidden, setHidden] = useState(true);
+
   return (
-    <div>
+    <>
       {
         customization.cloudinary.slice(customization.cloudinary.length - 3)
           === "mp4" ? (
-          <video alt={customization.whatsapp} className="Card" controls>
-            <source src={customization.cloudinary} type="video/mp4">
-            </source>
-          </video>
+          <div>
+            <div className="DataVisible">
+              <button
+                onClick={() => setHidden(!hidden)}
+              >
+                {
+                  hidden ?
+                    <span className="material-symbols-outlined">
+                      visibility_off
+                    </span> :
+                    <span className="material-symbols-outlined">
+                      visibility
+                    </span>
+                }
+              </button>
+              <span hidden={hidden}>
+                id: {customization.id}/  whatsapp: {customization.whatsapp}
+              </span>
+            </div>
+            <video alt={customization.whatsapp} className="Card" controls>
+              <source src={customization.cloudinary} type="video/mp4">
+              </source>
+            </video>
+          </div>
         ) : (
-          <img
-            className="Card"
-            data-testid={`customization__img-card-${customization.id}`}
-            src={customization.cloudinary || customization.figurinha}
-            alt={customization.whatsapp}
-          />
+          <div>
+            <div className="DataVisible">
+              <button
+                onClick={() => setHidden(!hidden)}
+              >
+                {
+                  hidden ?
+                    <span className="material-symbols-outlined">
+                      visibility_off
+                    </span> :
+                    <span className="material-symbols-outlined">
+                      visibility
+                    </span>
+                }
+              </button>
+              <span hidden={hidden}>
+                id:{customization.id}/   whatsapp:{customization.whatsapp}
+              </span>
+            </div>
+            <img
+              className="Card"
+              data-testid={`customization__img-card-${customization.id}`}
+              src={customization.cloudinary || customization.figurinha}
+              alt={customization.whatsapp}
+            />
+          </div>
         )
       }
-    </div>
+    </>
   );
 }
